646e20944608ae7a07c7c320222f7863dd0928e3
jesseyproctor <jss.proctor@gmail.com>
2020-11-17 21:52:29 -0500

    fixed-names

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const customersDiv = document.createElement('div')
 console.log(customersDiv)
 document.querySelector('body').appendChild(customersDiv)
 customersDiv.innerHTML = `<img src=${customers[0].picture.large}>` 
-customersDiv.innerHTML += `<h4>${customers[0].name}</h4>`
+customersDiv.innerHTML += `<h4>${customers[0].name.first + " " + customers[0].name.last}</h4>`
 customersDiv.innerHTML += `<p>${customers[0].email}</p>`
 customersDiv.innerHTML += `<p>${customers[0].location}</p>`
 customersDiv.innerHTML += `<p>${customers[0].dob.date}</p>`
@@ -22,7 +22,7 @@ const customersDiv2 = document.createElement('div')
 console.log(customersDiv2)
 document.querySelector('body').appendChild(customersDiv2)
 customersDiv2.innerHTML = `<img src=${customers[1].picture.large}>`
-customersDiv2.innerHTML += `<h4>${customers[1].name}</h4>`
+customersDiv2.innerHTML += `<h4>${customers[1].name.first + " " + customers[1].name.last}</h4>`
 customersDiv2.innerHTML += `<p>${customers[1].email}</p>`
 customersDiv2.innerHTML += `<p>${customers[1].location}</p>`
 customersDiv2.innerHTML += `<p>${customers[1].dob.date}</p>`
@@ -32,7 +32,7 @@ const customersDiv3 = document.createElement('div')
 console.log(customersDiv3)
 document.querySelector('body').appendChild(customersDiv3)
 customersDiv3.innerHTML = `<img src=${customers[2].picture.large}>`
-customersDiv3.innerHTML += `<h4>${customers[2].name}</h4>`
+customersDiv3.innerHTML += `<h4>${customers[2].name.first + " " + customers[2].name.last}</h4>`
 customersDiv3.innerHTML += `<p>${customers[2].email}</p>`
 customersDiv3.innerHTML += `<p>${customers[2].location}</p>`
 customersDiv3.innerHTML += `<p>${customers[2].dob.date}</p>`
@@ -42,7 +42,7 @@ const customersDiv4 = document.createElement('div')
 console.log(customersDiv4)
 document.querySelector('body').appendChild(customersDiv4)
 customersDiv4.innerHTML = `<img src=${customers[3].picture.large}>`
-customersDiv4.innerHTML += `<h4>${customers[3].name}</h4>`
+customersDiv4.innerHTML += `<h4>${customers[3].name.first + " " + customers[3].name.last}</h4>`
 customersDiv4.innerHTML += `<p>${customers[3].email}</p>`
 customersDiv4.innerHTML += `<p>${customers[3].location}</p>`
 customersDiv4.innerHTML += `<p>${customers[3].dob.date}</p>`
@@ -52,7 +52,7 @@ const customersDiv5 = document.createElement('div')
 console.log(customersDiv5)
 document.querySelector('body').appendChild(customersDiv5)
 customersDiv5.innerHTML = `<img src=${customers[4].picture.large}>`
-customersDiv5.innerHTML += `<h4>${customers[4].name}</h4>`
+customersDiv5.innerHTML += `<h4>${customers[4].name.first + " " + customers[4].name.last}</h4>`
 customersDiv5.innerHTML += `<p>${customers[4].email}</p>`
 customersDiv5.innerHTML += `<p>${customers[4].location}</p>`
 customersDiv5.innerHTML += `<p>${customers[4].dob.date}</p>`
@@ -62,7 +62,7 @@ const customersDiv6 = document.createElement('div')
 console.log(customersDiv6)
 document.querySelector('body').appendChild(customersDiv6)
 customersDiv6.innerHTML = `<img src=${customers[5].picture.large}>`
-customersDiv6.innerHTML += `<h4>${customers[5].name}</h4>`
+customersDiv6.innerHTML += `<h4>${customers[5].name.first + " " + customers[5].name.last}</h4>`
 customersDiv6.innerHTML += `<p>${customers[5].email}</p>`
 customersDiv6.innerHTML += `<p>${customers[5].location}</p>`
 customersDiv6.innerHTML += `<p>${customers[5].dob.date}</p>`
@@ -72,7 +72,7 @@ const customersDiv7 = document.createElement('div')
 console.log(customersDiv7)
 document.querySelector('body').appendChild(customersDiv7)
 customersDiv7.innerHTML = `<img src=${customers[6].picture.large}>`
-customersDiv7.innerHTML += `<h4>${customers[6].name}</h4>`
+customersDiv7.innerHTML += `<h4>${customers[6].name.first + " " + customers[6].name.last}</h4>`
 customersDiv7.innerHTML += `<p>${customers[6].email}</p>`
 customersDiv7.innerHTML += `<p>${customers[6].location}</p>`
 customersDiv7.innerHTML += `<p>${customers[6].dob.date}</p>`
@@ -82,7 +82,7 @@ const customersDiv8 = document.createElement('div')
 console.log(customersDiv8)
 document.querySelector('body').appendChild(customersDiv8)
 customersDiv8.innerHTML = `<img src=${customers[7].picture.large}>`
-customersDiv8.innerHTML += `<h4>${customers[7].name}</h4>`
+customersDiv8.innerHTML += `<h4>${customers[7].name.first + " " + customers[7].name.last}</h4>`
 customersDiv8.innerHTML += `<p>${customers[7].email}</p>`
 customersDiv8.innerHTML += `<p>${customers[7].location}</p>`
 customersDiv8.innerHTML += `<p>${customers[7].dob.date}</p>`
@@ -92,7 +92,7 @@ const customersDiv9 = document.createElement('div')
 console.log(customersDiv9)
 document.querySelector('body').appendChild(customersDiv9)
 customersDiv9.innerHTML = `<img src=${customers[8].picture.large}>`
-customersDiv9.innerHTML += `<h4>${customers[8].name}</h4>`
+customersDiv9.innerHTML += `<h4>${customers[8].name.first + " " + customers[8].name.last}</h4>`
 customersDiv9.innerHTML += `<p>${customers[8].email}</p>`
 customersDiv9.innerHTML += `<p>${customers[8].location}</p>`
 customersDiv9.innerHTML += `<p>${customers[8].dob.date}</p>`
@@ -102,7 +102,7 @@ const customersDiv10 = document.createElement('div')
 console.log(customersDiv10)
 document.querySelector('body').appendChild(customersDiv10)
 customersDiv10.innerHTML = `<img src=${customers[9].picture.large}>`
-customersDiv10.innerHTML += `<h4>${customers[9].name}</h4>`
+customersDiv10.innerHTML += `<h4>${customers[9].name.first + " " + customers[9].name.last}</h4>`
 customersDiv10.innerHTML += `<p>${customers[9].email}</p>`
 customersDiv10.innerHTML += `<p>${customers[9].location}</p>`
 customersDiv10.innerHTML += `<p>${customers[9].dob.date}</p>`
@@ -112,7 +112,7 @@ const customersDiv11 = document.createElement('div')
 console.log(customersDiv11)
 document.querySelector('body').appendChild(customersDiv11)
 customersDiv11.innerHTML = `<img src=${customers[10].picture.large}>`
-customersDiv11.innerHTML += `<h4>${customers[10].name}</h4>`
+customersDiv11.innerHTML += `<h4>${customers[10].name.first + " " + customers[10].name.last}</h4>`
 customersDiv11.innerHTML += `<p>${customers[10].email}</p>`
 customersDiv11.innerHTML += `<p>${customers[10].location}</p>`
 customersDiv11.innerHTML += `<p>${customers[10].dob.date}</p>`
@@ -122,7 +122,7 @@ const customersDiv12 = document.createElement('div')
 console.log(customersDiv12)
 document.querySelector('body').appendChild(customersDiv12)
 customersDiv12.innerHTML = `<img src=${customers[11].picture.large}>`
-customersDiv12.innerHTML += `<h4>${customers[11].name}</h4>`
+customersDiv12.innerHTML += `<h4>${customers[11].name.first + " " + customers[11].name.last}</h4>`
 customersDiv12.innerHTML += `<p>${customers[11].email}</p>`
 customersDiv12.innerHTML += `<p>${customers[11].location}</p>`
 customersDiv12.innerHTML += `<p>${customers[11].dob.date}</p>`
